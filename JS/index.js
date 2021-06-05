903d611149e85781ff15fca69baaaa9e2993fb53
Reily Stanford <reijstan@ut.utm.edu>
2021-06-04 23:00:27 -0500

    Updated index and people page. Added timer to index and centered bottom two people on people page.

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,20 @@
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        return document.querySelector('.navbar').classList.add('hide')
-    }
-    return document.querySelector('.navbar').classList.remove('hide')
-  
-  });
+window.onload = function countDown() {
+    var target = new Date("Jun 25, 2021 23:59:59").getTime();
+
+    var x = setInterval(function() {
+        var current = new Date().getTime();
+
+        var timeLeft = target - current;
+
+        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60  * 60));
+        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+
+        document.getElementById("Timer").innerHTML = "" + days + " Days " + hours + " Hours " + minutes + " Minutes";
+
+        if(timeLeft < 0) {
+            clearInterval(x);
+            document.getElementById("TimerContainer").innerHTML = "";
+        }
+    });
+}

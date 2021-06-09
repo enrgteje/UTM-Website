e798047cb60d4e411e60e0ccf424133068e7d434
Reily Stanford <reijstan@ut.utm.edu>
2021-06-08 23:43:07 -0500

    Worked on banner, timer, and lists.

--- JS/index.js
+++ JS/index.js
@@ -6,11 +6,11 @@ window.onload = function countDown() {
 
         var timeLeft = target - current;
 
-        var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var weeks = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 7));
+        var days = Math.floor(timeLeft % (1000 * 60 * 60 * 24 * 7) / (1000 * 60 * 60 * 24));
         var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60  * 60));
-        var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 
-        document.getElementById("Timer").innerHTML = "" + days + " Days " + hours + " Hours " + minutes + " Minutes";
+        document.getElementById("Timer").innerHTML = weeks + " Weeks " + days + " Days " + hours + " Hours";
 
         if(timeLeft < 0) {
             clearInterval(x);

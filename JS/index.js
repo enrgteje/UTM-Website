c61ba750d3fea2894161e6cef05437f24b19418e
Reily Stanford <reijstan@ut.utm.edu>
2021-08-05 22:22:58 -0500

    Updated footer on resource and events. Updated the information box on the home page.

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,26 @@
-window.onload = function countDown() {
-    var target = new Date("Jun 15, 2021 16:00:00").getTime();
+// window.onload = function countDown() {
+//     var target = new Date("Jun 15, 2021 16:00:00").getTime();
 
-    var x = setInterval(function() {
-        var current = new Date().getTime();
+//     var x = setInterval(function() {
+//         var current = new Date().getTime();
 
-        var timeLeft = target - current;
+//         var timeLeft = target - current;
 
-        var weeks = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 7));
-        var days = Math.floor(timeLeft % (1000 * 60 * 60 * 24 * 7) / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60  * 60));
+//         var weeks = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * 7));
+//         var days = Math.floor(timeLeft % (1000 * 60 * 60 * 24 * 7) / (1000 * 60 * 60 * 24));
+//         var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60  * 60));
 
 
-        if(weeks == 0){
-            document.getElementById("Timer").innerHTML = days + " Days " + hours + " Hours";
-        }
-        else
-            document.getElementById("Timer").innerHTML = weeks + " Weeks " + days + " Days " + hours + " Hours";
+//         if(weeks == 0){
+//             document.getElementById("Timer").innerHTML = days + " Days " + hours + " Hours";
+//         }
+//         else
+//             document.getElementById("Timer").innerHTML = weeks + " Weeks " + days + " Days " + hours + " Hours";
 
-        if(timeLeft < 0) {
-            clearInterval(x);
-            document.getElementById("TimerContainer").innerHTML = `<h1>CLASS is in session!</h1>
-            <a href="https://www.utm.edu/class">Register Now</a>`;
-        }
-    });
-}
+//         if(timeLeft < 0) {
+//             clearInterval(x);
+//             document.getElementById("TimerContainer").innerHTML = `<h1>CLASS is in session!</h1>
+//             <a href="https://www.utm.edu/class">Register Now</a>`;
+//         }
+//     });
+// }
